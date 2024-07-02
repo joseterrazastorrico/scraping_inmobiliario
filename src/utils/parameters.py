786ee66348182bf35ce9b2c5939c658a4f568cf1
joseterrazastorrico <jose.terrazas@gmail.com>
2024-07-02 14:41:15 -0400

    add parameters

---
+++ src/utils/parameters.py
@@ -0,0 +1,9 @@
+status = ['arriendo', 'venta']
+inmuebles = ['casa', 'departamento']
+comunas = [
+        'la-cisterna',
+        'las-condes' , 'providencia', 'ñuñoa', 'la-reina', 'macul', 'san-miguel', 'vitacura', 'lo-barnechea',
+        'independencia', 'estacion-central', 'san-joaquin', 'la-florida', 'puente-alto',
+        'santiago', 'cerrillos', 'maipu', 'recoleta', 'huechuraba', 'quilicura', 'colina', 'peñalolen'
+        ]
+regiones = ['metropolitana']
